9f643b50af3202ce82ad089493959a9534aa149e
Vahid Heidari <crystal.delta@gmail.com>
2016-12-18 22:39:58 -0800

    ClipVolumeInclined improved.

--- Lockon/LO_Viewer/Math/ClipVolumeInclined.h
+++ Lockon/LO_Viewer/Math/ClipVolumeInclined.h
@@ -6,9 +6,9 @@ class ClipVolumeInclined
 public: __thiscall ClipVolumeInclined::ClipVolumeInclined(class Box const &,class Vector3 const &,float)
 public: __thiscall ClipVolumeInclined::ClipVolumeInclined(class ClipVolumeInclined const &)
 public: __thiscall ClipVolumeInclined::ClipVolumeInclined(void)
+public: virtual __thiscall ClipVolumeInclined::~ClipVolumeInclined(void)
 public: class ClipVolumeInclined & __thiscall ClipVolumeInclined::operator=(class ClipVolumeInclined const &)
 public: enum IntersectionType __thiscall ClipVolumeInclined::IntersectOrientedBox(class Position3 const &,class Box const &)const 
-public: virtual __thiscall ClipVolumeInclined::~ClipVolumeInclined(void)
 public: virtual enum IntersectionType __thiscall ClipVolumeInclined::IntersectBox(float,float,float,float,float,float)
 public: virtual enum IntersectionType __thiscall ClipVolumeInclined::IntersectSquare(float,float,float,float)
 public: virtual void __thiscall ClipVolumeInclined::Prepare(float,float,float,float,int)

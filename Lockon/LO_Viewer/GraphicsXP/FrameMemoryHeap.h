4b72d0736fa60ecc8b98221dd67672a8634c9389
Vahid Heidari <crystal.delta@gmail.com>
2016-12-18 22:34:30 -0800

    FrameMemoryHeap improved.

--- Lockon/LO_Viewer/GraphicsXP/FrameMemoryHeap.h
+++ Lockon/LO_Viewer/GraphicsXP/FrameMemoryHeap.h
@@ -4,25 +4,26 @@
 class FrameMemoryHeap
 {
 	struct free_chunk {};
-class FrameMemoryHeap frame_heap
 
 public: __thiscall FrameMemoryHeap(int,int)
 public: __thiscall ~FrameMemoryHeap(void)
 public: class FrameMemoryHeap & __thiscall operator=(class FrameMemoryHeap const &)
-private: void __thiscall create_pool(int)
 public: void __thiscall free_memory(void *,unsigned int)
-private: struct free_chunk * __thiscall get_chunk(void *,int)
-private: void * __thiscall get_from_pool(unsigned int)
 public: int __thiscall get_max_memory(void)
 public: void * __thiscall get_memory(unsigned int)
-private: void __thiscall move_left(struct free_chunk *)
-private: void __thiscall move_right(struct free_chunk *)
 public: void * __thiscall quick_get_memory(unsigned int)
 public: void __thiscall release_memory(void)
-private: struct free_chunk * __thiscall remove_chunk(struct free_chunk *)
 public: void __thiscall reset_memory(void)
+private: void __thiscall create_pool(int)
+private: struct free_chunk * __thiscall get_chunk(void *,int)
+private: void * __thiscall get_from_pool(unsigned int)
+private: void __thiscall move_left(struct free_chunk *)
+private: void __thiscall move_right(struct free_chunk *)
+private: struct free_chunk * __thiscall remove_chunk(struct free_chunk *)
 public: void __thiscall FrameMemoryHeap::`default constructor closure'(void)
 };
+
+class FrameMemoryHeap frame_heap
 
 #endif
 

0779369380fef03457e0bd3d65134ff59ddfa0f3
Vahid Heidari <crystal.delta@gmail.com>
2016-12-22 16:30:17 -0800

    Weapons.h file removed.

--- Lockon/Src/Weapons/Weapons.h
+++
@@ -1,8 +0,0 @@
-#ifndef WEAPONS_H_
-#define WEAPONS_H_
-
-public: struct NURS_Const & __thiscall NURS_Const::operator=(struct NURS_Const const &)
-public: struct wship_WPU & __thiscall wship_WPU::operator=(struct wship_WPU const &)
-
-#endif
-
